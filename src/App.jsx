ee529f746f013465cec634defec3df1d720d04b3
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2024-10-23 21:58:33 +0100

    Commenting done partially

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
 function App() {
+  /* movie state variable for the movies being pulled from the API so we can access their properties
+  Movie search state variable which will be passed into the API url as a string from our search
+  bar when the user inputs.
+  favourites state variable for us to add functionality to favourite, unfavourite a chosen movie.
+  */
   const [movies, setMovies] = useState([]);
   const [movieSearch, setMovieSearch] = useState("");
   const [favourites, setFavourites] = useState([]);
 
+
 useEffect(() => {
+  // passes in our empty string to the getMovie method as the component first mounts which will return nothing
   getMoviesApiRequest(movieSearch);
+  // dependeny to re-render and make the method call everytime the movieSearch value changes
 }, [movieSearch]);
 
 useEffect(() => {
+  try {
+    // converts the string into a json object from the local storage
   const existingFavouritesList = JSON.parse(localStorage.getItem("react-movie-app-favourites-list"));
+  // checking so that even if our exitsing list is null it doesn't cause an error
   if (existingFavouritesList) {
     setFavourites(existingFavouritesList);
   } else {
     setFavourites([]);
   }
+  } catch (error) {
+    console.log(error);
+  }
+  // runs when the component mounts and has no dependencies
 }, []) 
 
+// method to save our favourites list to local storage
 function savetoLocalStorage(items) {
   localStorage.setItem("react-movie-app-favourites-list", JSON.stringify(items));
 }
 
-
+// async function to make the api call
 const getMoviesApiRequest = async (movieSearch) => {
   try {
+    // to modify search to fit user input
     const apiUrl = `https://www.omdbapi.com/?s=${movieSearch}&apikey=f43f88af`;
     const response = await fetch(apiUrl);
 
@@ -41,26 +58,35 @@ const getMoviesApiRequest = async (movieSearch) => {
 
     const movieData = await response.json();
 
+    // only if a movie inputs a Search object we add it to our setMovies updater state functiom
     if (movieData.Search) {
       setMovies(prevMovieSearch => (movieData.Search)); 
     }
     
   }
     catch (error) {
-      console.error(error);
       console.log(error);
   }
 }
 
+// method to add movie we click on to our favourites
 function addFavouriteMovie(movie) {
+  // keeps the old array of previous favourites using the spread operator so we don't lose them
   const newFavouriteMoviesList = [...favourites, movie];
+
   setFavourites(prevFavsList => (newFavouriteMoviesList));
+  // add to local storage
   savetoLocalStorage(newFavouriteMoviesList);
 }
+
+
+// removes favourites upon click
 function removeFavouriteMovie(movie) {
   const updatedFavouritesList = favourites.filter((favourite) => 
     (favourite.imdbID !== movie.imdbID));
+
   setFavourites(prevFavsList => (updatedFavouritesList));
+  // add to local storage 
   savetoLocalStorage(updatedFavouritesList);
 }
 
@@ -68,10 +94,10 @@ function removeFavouriteMovie(movie) {
     <div className="container-fluid movie-app">
       <div className="row align-items-center mt-2 mb-0">
         <div className="col title-bar">
-          <TitleBar heading="MovieSearcherPro" />
+          <TitleBar heading="MoovySearcherPro" />
         </div>
         <div className="col-auto">
-          <SearchBar movieSearch={movieSearch} setMovieSearch={setMovieSearch} />
+          <SearchBar movieSearch={movieSearch} setMovieSearch={setMovieSearch} /> 
         </div>
       </div>
       <div className="row mt-4">
